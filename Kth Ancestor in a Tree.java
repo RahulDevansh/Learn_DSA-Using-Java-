6f4081f87821e0fba27eee1cc2e8496dffb30465
Rahul Kumar <138897953+RahulDevansh@users.noreply.github.com>
2023-07-26 23:21:11 +0530

    Create Kth Ancestor in a Tree.java

---
+++ Kth Ancestor in a Tree.java
@@ -0,0 +1,26 @@
+class Solution
+{
+    int ans ;
+    public int kthAncestor(Node root, int k, int node)
+    {
+        //Write your code here
+        ArrayList<Integer> al = new ArrayList<>();
+        helper(root,k,node,al);
+        return ans;
+    }
+    
+    void helper(Node root,int k, int node, ArrayList<Integer> al) {
+        if(root== null) {
+            return;
+        }
+        if(root.data == node) {
+            if(al.size() < k)    ans =-1;
+            else    ans = al.get(al.size()-k);
+            return;
+        } 
+        al.add(root.data);
+        helper(root.left,k,node,al);
+        helper(root.right,k,node,al);
+        al.remove(al.size()-1);
+    }
+}
