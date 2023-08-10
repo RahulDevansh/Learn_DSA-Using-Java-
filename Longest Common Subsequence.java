ff629435b5b139c9c1d985ebe9bb26b8f6c1a700
Rahul Kumar <138897953+RahulDevansh@users.noreply.github.com>
2023-08-10 23:47:17 +0530

    Create Longest Common Subsequence.java

---
+++ Longest Common Subsequence.java
@@ -0,0 +1,30 @@
+//https://practice.geeksforgeeks.org/problems/longest-common-subsequence-1587115620/1
+/*
+A = 6, B = 6
+str1 = ABCDGH
+str2 = AEDFHR
+Output: 3
+Explanation: LCS for input strings “ABCDGH” and “AEDFHR” is “ADH” of length 3.
+*/
+class Solution
+{
+    //Function to find the length of longest common subsequence in two strings.
+    static int lcs(int x, int y, String s1, String s2)
+    {
+        // your code here
+        int[][] dp = new int[x+1][y+1];
+        
+        for(int i=1;i<=x;i++){
+            for(int j=1;j<=y;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[x][y];
+    }
+    
+}
