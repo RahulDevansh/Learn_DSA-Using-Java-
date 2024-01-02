1e17bd380e27a0ef948c4e2e3f6168121e809215
Rahul Kumar <138897953+RahulDevansh@users.noreply.github.com>
2024-01-02 06:15:12 -0800

    Create Largest Sum Subarray of Size at least K.java

---
+++ Largest Sum Subarray of Size at least K.java
@@ -0,0 +1,29 @@
+// https://www.geeksforgeeks.org/problems/largest-sum-subarray-of-size-at-least-k3121/1 
+class Solution {
+    
+    public long maxSumWithK(long a[], long n, long k)
+    {
+        long sum=0;
+        for (int i=0;i<k;i++){
+            sum+=a[i];
+        }
+        if (n==k){
+            return sum;
+        }
+        long max=sum;
+        long initial=0;
+        for (int i=(int)k;i<n;i++){
+            sum+=a[i];
+            sum-=a[i-(int)k];
+            initial+=a[i-(int)k];
+            if (initial>=0){
+                max=Math.max(sum+initial,max);
+            }
+            else{
+                max=Math.max(max,sum);
+                initial=0;
+            }
+        }
+        return max;
+    }
+}
