ba520ef6acb59743235fde70ba1990cef887c559
Rahul Kumar <138897953+RahulDevansh@users.noreply.github.com>
2024-01-05 07:56:59 -0800

    Create Count possible ways to construct buildings.java

---
+++ Count possible ways to construct buildings.java
@@ -0,0 +1,18 @@
+// https://www.geeksforgeeks.org/problems/count-possible-ways-to-construct-buildings5007/1
+class Solution
+{
+    public int TotalWays(int N)
+    {
+        long mod=1000000007;
+    long  ans=0,curr=1, prev=1;
+        for(int i=1;i<=N;i++)
+        {
+            ans=(prev+curr)%mod; 
+            prev=curr;
+            curr=ans;
+        }
+
+        ans= (ans*ans)%mod;
+        return (int)ans;
+    }
+};
