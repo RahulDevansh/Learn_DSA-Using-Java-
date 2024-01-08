81cdcadc5466895897938355031d52847bdd66ef
Rahul Kumar <138897953+RahulDevansh@users.noreply.github.com>
2024-01-08 07:51:10 -0800

    Create Merge 2 sorted linked list in reverse order.java

---
+++ Merge 2 sorted linked list in reverse order.java
@@ -0,0 +1,45 @@
+// https://www.geeksforgeeks.org/problems/merge-2-sorted-linked-list-in-reverse-order/1
+/* Structure of the node*/
+/* class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+} */
+
+class GfG{
+    Node mergeResult(Node list1, Node list2){
+        
+        // Merge 2 sorted lists -> Ascending order
+        Node head = mergeTwoLists(list1, list2);
+        
+        // Using the head pointer, reverse the linked list
+        Node newHead = null;
+        while (head != null) {
+            Node nextNode = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = nextNode;
+        }
+        
+        return newHead;
+    }
+    
+    public Node mergeTwoLists(Node list1, Node list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        if (list1.data < list2.data) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
