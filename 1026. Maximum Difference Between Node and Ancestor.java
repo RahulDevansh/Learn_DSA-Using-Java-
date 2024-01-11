ee2e230ff204f97da528a0aaeab66b0f4554aa23
Rahul Kumar <138897953+RahulDevansh@users.noreply.github.com>
2024-01-11 07:47:46 -0800

    Create 1026. Maximum Difference Between Node and Ancestor.java

---
+++ 1026. Maximum Difference Between Node and Ancestor.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
+class Solution {
+    int diff=Integer.MIN_VALUE;
+    public int maxAncestorDiff(TreeNode root) {
+        dfs(root,root.val,root.val);
+        return diff;
+    }
+    public void dfs(TreeNode root,int max,int min){
+        if(root==null)
+            return;
+        max=Math.max(max,root.val);
+        min=Math.min(min,root.val);
+        diff=Math.max(diff,max-min);
+        dfs(root.left,max,min);
+        dfs(root.right,max,min);
+    }
+}
